5d923bb620e9d4f487422c311f1ff93e17bea6a5
Carl Smith <carlfsmithiii@gmail.com>
2018-11-19 03:51:42 -1000

    Add get_special_paths() -- find special files in given directory

--- copyspecial.py
+++ copyspecial.py
@@ -10,7 +10,8 @@ import sys
 import re
 import os
 import shutil
-import commands
+# import commands   
+import subprocess
 import argparse
 
 """Copy Special exercise
@@ -19,12 +20,16 @@ import argparse
 # +++your code here+++
 # Write functions and modify main() to call them
 
+
 def main():
     # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
-    parser.add_argument('--todir', help='dest dir for special files')
+    parser.add_argument('--todir', default=os.getcwd(),
+                        help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
     # TODO need an argument to pick up 'from_dir'
+    parser.add_argument('--fromdir', default=os.getcwd(),
+                        help='source dir for special files')
     args = parser.parse_args()
 
     # TODO you must write your own code to get the cmdline args.
@@ -36,6 +41,21 @@ def main():
 
     # +++your code here+++
     # Call your functions
-  
+    special_filenames = get_special_paths(args.fromdir)
+
+
+def get_special_paths(dir):
+    filenames = os.listdir(dir)
+    special_filenames = (os.path.join(dir, f)
+                         for f in filenames if re.search(r'__\w+__', f))
+    return special_filenames
+
+
+def copy_to(paths, dir):
+    for filepath in paths:
+        shutil.copy(filepath, dir)
+
+
+
 if __name__ == "__main__":
     main()
